f1a4f41739ff3551c0a48fa24e94d46c2276ccb8
Symeon Melnychuk <m.simeon998@gmail.com>
2023-07-20 10:31:39 +0300

    5

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,21 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { contactReducer } from './contactSlice';
 import { filterReducer } from './filterSlice';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const persistAddContact = persistReducer(persistConfig, contactReducer);
 
 export const store = configureStore({
   reducer: {
-    contacts: contactReducer,
+    contacts: persistAddContact,
     filters: filterReducer,
   },
 });
+
+export const persistor = persistStore(store);
